c8c6e04575a0d11d5293455b517922dc213de546
ligang <sogang@163.com>
2020-01-06 09:45:55 +0800

    支持markdown

--- src/main/resources/db/migration/V9__Create_notification_table.sql
+++ src/main/resources/db/migration/V9__Create_notification_table.sql
@@ -6,7 +6,7 @@ create table notification
 	outerId bigint not null,
 	type int not null,
 	gmt_create bigint not null,
-	type int default 0 not null,
+	status int default 0 not null,
 	notifier_name varchar(100) not null,
 	outer_title varchar(256) not null,
 	constraint notification_pk
